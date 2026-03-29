4ab6d4184e98361cb05330f77c9d2be43080742e
Ticua <ticua@ticua.ar>
2026-03-29 19:35:10 -0300

    Update to 26.1

--- src/main/java/io/github/ticua07/stringduperfix/mixin/TripwireHookBlockMixin.java
+++ src/main/java/io/github/ticua07/stringduperfix/mixin/TripwireHookBlockMixin.java
@@ -2,25 +2,25 @@ package io.github.ticua07.stringduperfix.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.TripwireHookBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.TripWireHookBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Slice;
 
-// code from https://github.com/WinExp/DumpedCarpetAddition/blob/1.21.3/src/main/java/com/github/winexp/dumpedcarpetaddition/mixins/largeOakGenerationFix/TripwireHookBlockMixin.java
-
-@Mixin(TripwireHookBlock.class)
+@Mixin(TripWireHookBlock.class)
 public abstract class TripwireHookBlockMixin {
+
     @WrapOperation(
-            method = "update",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"),
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1)
+            method = "calculateState",
+            at = @At(
+                    value = "INVOKE",
+                    //target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z",
+                    target = "Lnet/minecraft/world/level/block/state/BlockState;is(Ljava/lang/Object;)Z",
+                    ordinal = 3
             )
     )
-    private static boolean reintroduceStringDuper(BlockState instance, Block block, Operation<Boolean> original) {
+    private static boolean reintroduceStringDuper(BlockState instance, Object o, Operation<Boolean> original) {
         return true;
     }
 }
